4430e3042b47ef962a5c846c5a3a1bf4678a6887
faryal_junaid <162690320+faryal16@users.noreply.github.com>
2024-04-08 06:21:02 +0500

    updated files

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-#! usr/bin/evn node
+#! /usr/bin/env node
 import inquirer from "inquirer";
-let todos = [];
-let condition = true;
-while (condition) {
-    let answer = await inquirer.prompt([
+let myBalance = 10000;
+let myPinCode = 12345;
+let pinAns = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number",
+    },
+]);
+if (pinAns.pin === myPinCode) {
+    console.log("Correct pincode!!!");
+    let operationAns = await inquirer.prompt([
         {
-            name: "todo",
-            type: "input",
-            message: "what do you want to add in yours todos?"
+            name: "operation",
+            message: "select your operation",
+            type: "list",
+            choices: ["withdraw", "checkbalance", "deposite"],
         },
-        {
-            name: "addmore",
-            type: "confirm",
-            message: "Do you want to add more?",
-            default: "true"
-        }
     ]);
-    const { todo, addmore } = answer;
-    condition = addmore;
-    if (todo) {
-        todos.push(todo);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number",
+            },
+        ]);
+        myBalance -= amountAns.amount;
+        if (amountAns.amount <= 10000) {
+            console.log(`your remaining amount is ${myBalance} $`);
+        }
+        else {
+            console.log("Please enter a valid amount you have only 10000");
+        }
     }
-    else {
-        console.log("Kindly enter a valid input.");
+    else if (operationAns.operation === "checkbalance") {
+        console.log(`your balance is: ${myBalance}$`);
     }
-}
-if (todos.length > 0) {
-    console.log("Your todo list:");
-    todos.forEach(todo => {
-        console.log(todo);
-    });
+    else if (operationAns.operation === "deposite") {
+        let cashAns = await inquirer.prompt([
+            { name: "cash",
+                message: "which amount you want to deposite",
+                type: "list",
+                choices: [5000, 10000, 15000, 20000],
+            }
+        ]);
+        console.log(`today you deposite ${cashAns.cash} cash in your bank account so now your total amount is ${myBalance + cashAns.cash}`);
+    }
 }
 else {
-    console.log("No todo's found.");
+    console.log("INcorrect Pin!!");
 }
--- index.ts
+++ index.ts
@@ -1,46 +1,62 @@
-#! usr/bin/evn node
-import inquirer from "inquirer"
-let todos=[]; 
+#! /usr/bin/env node
+import inquirer from "inquirer";
 
-let condition=true;
-while(condition)
-{
-let answer:
-{
-    todo:string,
-    addmore:boolean,
-}= await inquirer.prompt (
-    [
-        {
-            name:"todo",
-            type:"input",
-            message:"what do you want to add in yours todos?"
-        },
-        {
-            name:"addmore",
-            type:"confirm", // when type is confirm answer in yes or no.
-            message:"Do you want to add more?",
-            default:"true"
-        }
-    ]
-);
-const {todo, addmore}= answer;
-condition=addmore;
-if(todo){
-         todos.push(todo)
-}else{
-    console.log("Kindly enter a valid input.")
-}
-}
-if (todos.length > 0){
-    console.log("Your todo list:");
-    todos.forEach(todo =>{
-        console.log(todo)
+let myBalance = 10000;
 
+let myPinCode = 12345;
+
+let pinAns = await inquirer.prompt([
+  {
+    name: "pin",
+    message: "enter your pin",
+    type: "number",
+  },
+]);
+if (pinAns.pin === myPinCode) {
+  console.log("Correct pincode!!!");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "select your operation",
+      type: "list",
+      choices: ["withdraw", "checkbalance","deposite"],
+    },
+  ]);
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "enter your amount",
+        type: "number",
+      },
+    ]);
+    myBalance -= amountAns.amount
+    if(amountAns.amount <=10000 ){
+    console.log(`your remaining amount is ${myBalance} $`);
+    }else{
+      console.log("Please enter a valid amount you have only 10000");
+      
     }
-    );
+  }
+  else if(operationAns.operation === "checkbalance"){
+    console.log(`your balance is: ${myBalance}$` );
     
-
+  }
+  else if(operationAns.operation === "deposite"){
+    let cashAns = await inquirer.prompt(
+    [
+      {name:"cash",
+       message:"which amount you want to deposite",
+       type:"list",
+       choices:[5000,10000,15000,20000],
+      }   
+    ]
+    )
+    console.log(`today you deposite ${cashAns.cash} cash in your bank account so now your total amount is ${myBalance + cashAns.cash}`);
+    
+  }
+  
 }else{
-    console.log("No todo's found.")
-}
+    console.log("INcorrect Pin!!")
+  }
